9e647ed45453d6ccb5c6a4ae55d880daa56f4a45
Stéphane Besnard <besnard_stephane@yahoo.com>
2025-06-04 08:13:03 +0200

    Corrected hole in if + absolute link

--- svgtag/shape2svg.py
+++ svgtag/shape2svg.py
@@ -1,31 +1,13 @@
-from .svgprocess import SVG
+from svgtag.svgprocess import SVG
 
 
-def shape_svg(width, height, thk, shape, hole=0):
+def shape_svg(width, height, thk, shape=None, hole=None):
     svg = SVG()  # Créer un nouvel objet SVG
     x0 = 0
     y0 = 0
 
     svg.height = height
-    if hole == 0:
-        svg.width = width
-        svg.viewBox = [0, 0, width, height]
-        # Ajout d'un rectangle directement à l'objet SVG
-        # svg.add_element('rect', {
-        #     'x': x0,
-        #     'y': y0,
-        #     'width': width,
-        #     'height': height,
-        #     'fill': 'none',
-        #     'stroke': 'black',
-        #     'stroke-width': thk
-        # })
-        path_d = f"M {x0} {y0} h {width} v {height} h {-width} v {-height} z"
-        svg.add_element(
-            "path", {"d": path_d, "fill": "none", "stroke": "black", "stroke-width": thk}
-        )
-
-    else:
+    if hole and shape:
         svg.width = width + height / 2
         svg.viewBox = [-height / 2, 0, width + height / 2, height]
         path_d = f"M {x0} {y0} h {width} v {height} h {-width} "
@@ -50,6 +32,25 @@ def shape_svg(width, height, thk, shape, hole=0):
         svg.add_element(
             "path", {"d": path_d, "fill": "none", "stroke": "black", "stroke-width": thk}
         )
+
+    else:
+        svg.width = width
+        svg.viewBox = [0, 0, width, height]
+        # Ajout d'un rectangle directement à l'objet SVG
+        # svg.add_element('rect', {
+        #     'x': x0,
+        #     'y': y0,
+        #     'width': width,
+        #     'height': height,
+        #     'fill': 'none',
+        #     'stroke': 'black',
+        #     'stroke-width': thk
+        # })
+        path_d = f"M {x0} {y0} h {width} v {height} h {-width} v {-height} z"
+        svg.add_element(
+            "path", {"d": path_d, "fill": "none", "stroke": "black", "stroke-width": thk}
+        )
+
 
     svg.update_svg_content()
     return svg
